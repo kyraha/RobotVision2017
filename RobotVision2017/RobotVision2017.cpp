a4ee24cbf53e1927416546caa9df8ce1acc02664
Mikhail Kyraha <mikhail@kyraha.com>
2016-11-10 16:59:33 -0600

    Add project files.

---
+++ RobotVision2017/RobotVision2017.cpp
@@ -0,0 +1,12 @@
+// RobotVision2017.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "opencv2/highgui.hpp"
+
+
+int main(int argc, char **argv)
+{
+	printf("Hello CV\n");
+	cv::waitKey();
+}
