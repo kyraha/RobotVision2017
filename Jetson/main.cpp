44f027511c9ae9668bc9bbc6fe98e9b7d650c42f
Mikhail Kyraha <mikhail@kyraha.com>
2016-12-26 17:35:23 -0600

    Removed junk from the code left over from older tests

--- Jetson/main.cpp
+++ Jetson/main.cpp
@@ -5,30 +5,12 @@
 
 int main(int argc, char** argv)
 {
-#if 0
-	cv::Mat img(RobotVideo::CAPTURE_ROWS, RobotVideo::CAPTURE_COLS, CV_8UC3, cv::Scalar(0));
-	cv::putText(img,
-		"Hello World on Jetson!",
-		cv::Point(10, img.rows / 2),
-		cv::FONT_HERSHEY_DUPLEX,
-		1.0,
-		CV_RGB(118, 185, 0),
-		2);
-#endif
+	const cv::Size dispSize(848, 480);
+	int fontScale = 2 * RobotVideo::CAPTURE_ROWS / dispSize.height;
 
-	const int dispX = 848, dispY = 480;
-	int fontScale = 2 * RobotVideo::CAPTURE_ROWS / dispY;
-
-	if(argc < 2) {
-		std::cerr << "Image file name?" << std::endl;
-		return 1;
-	}
-
-	cv::Mat img = cv::imread(argv[1]);
-	cv::Size dispSize(dispX, dispY);
+	cv::Mat img;
 	cv::Mat display;
 
-	RobotVideo* processor = RobotVideo::GetInstance();
 	std::ostringstream capturePipe;
 	capturePipe << "nvcamerasrc ! video/x-raw(memory:NVMM)"
 		<< ", width=(int)" << RobotVideo::CAPTURE_COLS
@@ -41,39 +23,12 @@ int main(int argc, char** argv)
 //gst-launch-1.0 nvcamerasrc fpsRange="30.0 30.0" ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1' ! nvtee ! nvvidconv flip-method=2 ! 'video/x-raw(memory:NVMM), format=(string)I420' ! nvoverlaysink -e
 //cv::VideoCapture capture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
 
-#if 0
-	//open the video stream and make sure it's opened
-	//We specify desired frame size and fps in constructor
-	//Camera must be able to support specified framesize and frames per second
-	//or this will set camera to defaults
-	int count=1;
-	//while (!capture.open(CAPTURE_PORT, CAPTURE_COLS, CAPTURE_ROWS, CAPTURE_FPS))
-	//while (!capture.open(0))
-
-	{
-		std::cerr << "Error connecting to camera stream, retrying " << count<< std::endl;
-		if(count++ > 3) {
-			std::cerr << "Couldn't connect to camera" << std::endl;
-			return 1;
-		}
-		usleep(5 * 1000000);
-	}
-	capture.set(CV_CAP_PROP_FRAME_WIDTH, RobotVideo::CAPTURE_COLS);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, RobotVideo::CAPTURE_ROWS);
-
-	//After Opening Camera we need to configure the returned image setting
-	//all opencv v4l2 camera controls scale from 0.0 to 1.0
-	//capture.set(CV_CAP_PROP_EXPOSURE_ABSOLUTE, 0);
-	//capture.set(CV_CAP_PROP_BRIGHTNESS, 0);
-	//capture.set(CV_CAP_PROP_CONTRAST, 0);
-#else
 	if(!capture.isOpened()) {
 		std::cerr << "Couldn't connect to camera" << std::endl;
 		return 1;
 	}
 
-#endif
-
+	RobotVideo* processor = RobotVideo::GetInstance();
 	std::vector<double> total_times;
 	double total_start = double(clock())/CLOCKS_PER_SEC;
 	for(size_t n = 1;; ++n) {
@@ -139,11 +94,10 @@ int main(int argc, char** argv)
 			cv::putText(display, osst.str(), cv::Point(20,160+i*16), 1, 1, cv::Scalar(90,255,90), 1);
 		}
 
-		if(n%30 == 0) cv::imshow("Hello!", display);
+		if(n%60 == 0) cv::imshow("Hello!", display);
 
 		int key = 0xff & cv::waitKey(5);
 		if ((key & 255) == 27) break;
-		//if ((key & 255) == 32 && p_file != files.end()) filename = *p_file++;
 		if ((key & 255) == 32 ) {
 			cv::waitKey();
 		}
