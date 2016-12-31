d70f7242d9d267d7faf77fe5d3a3448b4212b4bd
Mikhail Kyraha <mikhail@kyraha.com>
2016-12-31 01:42:55 -0600

    Eclipse project for Linux cross-compile

--- Jetson/Video.cpp
+++ Jetson/Video.cpp
@@ -1,8 +1,8 @@
 #include "Misc/Video.h"
 #include <iostream>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
 #include "opencv2/cudaarithm.hpp"
 #include "opencv2/cudaimgproc.hpp"
 
